e7e1c74737da0c8b68188927be684d453a8aaaee
Jensssen <soeren.erichsen@gmx.de>
2024-04-04 23:08:34 +0200

    add iam roles

--- question1/terraform/modules/ecs/data.tf
+++ question1/terraform/modules/ecs/data.tf
@@ -1,10 +1,38 @@
 data "aws_iam_policy_document" "assume_role_policy" {
   statement {
+    effect  = "Allow"
     actions = ["sts:AssumeRole"]
 
     principals {
       type        = "Service"
       identifiers = ["ecs-tasks.amazonaws.com"]
     }
+  }
+}
+
+data "aws_iam_policy_document" "sagemaker_access" {
+  # Enables container debugging via ssh
+  statement {
+    effect    = "Allow"
+    actions   = ["ssmmessages:CreateControlChannel",
+                "ssmmessages:CreateDataChannel",
+                "ssmmessages:OpenControlChannel",
+                "ssmmessages:OpenDataChannel"]
+    resources = ["*"]
+  }
+  # Allow sagemaker endpoint access following least privileges
+  statement {
+    effect    = "Allow"
+    actions   = ["sagemaker:InvokeEndpoint",
+                "sagemaker:DescribeEndpoint",
+                "sagemaker:DescribeEndpointConfig",
+                "sagemaker:ListInferenceComponents",
+                "sagemaker:ListTags"]
+    resources = ["*"]
+  }
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:GetObject"]
+    resources = ["*"]
   }
 }
